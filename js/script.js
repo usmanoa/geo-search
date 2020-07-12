77b80fbecd96f9498bf72792e39a10f9b13b4f82
usmanoa <usmanakorede@gmail.com>
2020-07-12 13:29:08 +0100

    Add static map to page

--- js/script.js
+++ js/script.js
@@ -13,11 +13,41 @@ const geoCodePlace = async (location, apiKey) => {
     return  data.results[0].locations[0].latLng
 }
 
+/**
+ * Get the address of static map image
+ * 
+ * @param {float} lat - latitude of location
+ * @param {float} lng - longitude of location
+ * @param {string} apiKey - API key of MaqQuest
+ * 
+ * @return {string} url of static map image
+ */
+const getStaticMapUrl = (lat, lng, apiKey) => {
+    const url = `http://www.mapquestapi.com/staticmap/v5/map?key=`
+        + `${apiKey}&type=map&size=600,400&locations=${lat},${lng}|marker-sm-50318A-1`
+        + `&scalebar=true&zoom=5&rand=-13&traffic=flow|cons|inc45253154`
+    
+    return url
+}
+
 const form = document.querySelector('.form')
 const searchInput = document.querySelector('.form__input-search')
+const map = document.querySelector('.map')
 
 form.addEventListener('submit', (event) => {
     event.preventDefault();
     place = searchInput.value.trim()
-    console.log(place)
+
+    if(!place) {
+        return alert('There has been an error')  
+    }
+
+    const mapQuestApi = prompt('Enter the MapQuest Api Key')
+    
+    geoCodePlace(place, mapQuestApi)
+        .then(coord => {
+            const mapUrl = getStaticMapUrl(coord.lat, coord.lng, mapQuestApi)
+            map.innerHTML = `<img src=${mapUrl}, alt="map"/>`
+        })
+    
 })
