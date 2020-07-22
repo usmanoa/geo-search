018637ebb5d8ff60363a59f070494b57037248eb
usmanoa <usmanakorede@gmail.com>
2020-07-22 20:56:56 +0100

    Add minify task for files

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,33 @@
-const { dest, src } = require('gulp');
+const { dest, parallel, src } = require('gulp');
+const htmlmin = require('gulp-htmlmin');
+const csso = require('gulp-csso');
 const eslint = require('gulp-eslint');
+const uglify = require('gulp-uglify-es').default;
 
-function processHTML(cb) {
-    src('index.html')
+function processHTML() {
+    return src('*.html')
+        .pipe(htmlmin({
+            collapseWhitespace: true,
+            removeComments: true,
+        }))
         .pipe(dest('build'));
-    cb();
 }
 
-function processJS(cb) {
-    src('js/*.js')
-        .pipe(dest('build'));
-    cb();
+function processCSS() {
+    return src('css/*.css')
+        .pipe(csso())
+        .pipe(dest('build/css'));
+}
+
+function processAssets() {
+    return src('assets/*.*')
+        .pipe(dest('build/assets'));
+}
+
+function processJS() {
+    return src('js/*.js')
+        .pipe(uglify())
+        .pipe(dest('build/js'));
 }
 
 function lint() {
@@ -23,3 +40,4 @@ function lint() {
 exports.processHTML = processHTML;
 exports.processJS = processJS;
 exports.lint = lint;
+exports.build = parallel(processHTML, processCSS, processAssets, processJS);
