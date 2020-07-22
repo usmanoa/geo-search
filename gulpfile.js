b9652ef000ae72510ce369d8b37650b8b18393bb
usmanoa <usmanakorede@gmail.com>
2020-07-22 16:46:36 +0100

    Add airbnb eslint rules

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,25 @@
-function defaultTask(cb) {
-    // place code for your default task here
+const { dest, src } = require('gulp');
+const eslint = require('gulp-eslint');
+
+function processHTML(cb) {
+    src('index.html')
+        .pipe(dest('build'));
     cb();
 }
-  
-exports.default = defaultTask
+
+function processJS(cb) {
+    src('js/*.js')
+        .pipe(dest('build'));
+    cb();
+}
+
+function lint() {
+    return src('js/*.js')
+        .pipe(eslint())
+        .pipe(eslint.format())
+        .pipe(eslint.failOnError());
+}
+
+exports.processHTML = processHTML;
+exports.processJS = processJS;
+exports.lint = lint;
