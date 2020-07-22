d1474639a935cc089355005aef6f5eb0ce2bd366
usmanoa <usmanakorede@gmail.com>
2020-07-22 23:30:08 +0100

    Change file structure to include src folder

--- gulpfile.js
+++ gulpfile.js
@@ -8,25 +8,25 @@ const uglify = require('gulp-uglify-es').default;
 const del = require('del');
 const browserSync = require('browser-sync').create();
 
-const processHTML = () => src('*.html')
+const processHTML = () => src('src/*.html')
     .pipe(htmlmin({
         collapseWhitespace: true,
         removeComments: true,
     }))
     .pipe(dest('build'));
 
-const processCSS = () => src('css/*.css')
+const processCSS = () => src('src/css/*.css')
     .pipe(csso())
     .pipe(dest('build/css'));
 
-const processAssets = () => src('assets/*.*')
+const processAssets = () => src('src/assets/*.*')
     .pipe(dest('build/assets'));
 
-const processJS = () => src('js/*.js')
+const processJS = () => src('src/js/*.js')
     .pipe(uglify())
     .pipe(dest('build/js'));
 
-const lint = () => src('js/*.js')
+const lint = () => src('src/js/*.js')
     .pipe(eslint())
     .pipe(eslint.format())
     .pipe(eslint.failOnError());
@@ -35,7 +35,7 @@ const clean = () => del(['build']);
 
 const browserReload = (cb) => {
     browserSync.init({
-        server: './',
+        server: './src',
         port: 8080,
         ui: {
             port: 8081,
@@ -46,7 +46,7 @@ const browserReload = (cb) => {
 
 const watchFiles = () => {
     watch('**/*.html').on('change', browserSync.reload);
-    watch('*.css').on('change', browserSync.reload);
+    watch('**/*.css').on('change', browserSync.reload);
     watch('**/*.js').on('change', browserSync.reload);
 };
 
